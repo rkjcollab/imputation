8f10c287134ada2b18833ac349534d0b8aac695f
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2026-07-06 11:35:24 -0600

    Simplify for current platform match and lint

--- scripts/get_strand_flip_snp_names.R
+++ scripts/get_strand_flip_snp_names.R
@@ -11,98 +11,107 @@ print("where one has an allele mismatch, both variants are")
 print("removed due to inability to identify which of the")
 print("pair is correct.")
 
+# TODO: when revisit and rewrite, remove imp_server arg
+
 get_strand_flip_snp_names <- function(pre_qc_dir, post_qc_dir, imp_server) {
   # Load snp-excluded.txt
-  snp.excl <- read.delim(paste0(pre_qc_dir, "/snps-excluded.txt"),
-                         stringsAsFactors = F)
-  
+  snp_excl <- read.delim(paste0(pre_qc_dir, "/snps-excluded.txt"),
+    stringsAsFactors = FALSE
+  )
+
   # Add column with chr:pos for all variants
-  if (imp_server == "topmed") {
-    print("tm")
-    snp.excl$chr <- 
-      gsub("chr", "", unlist(strsplit(snp.excl$X.Position, split=":"))[seq(1,dim(snp.excl)[1]*4,4)])
-    snp.excl$pos <-
-      unlist(strsplit(snp.excl$X.Position, split=":"))[seq(2,dim(snp.excl)[1]*4,4)]
-    snp.excl$chr.pos <- paste0(snp.excl$chr, ":", snp.excl$pos)
-  } else if (grepl("mich", imp_server)) {
-    snp.excl$chr.pos <- paste0(snp.excl$CHROM, ":", snp.excl$POS)
-    snp.excl$FilterType <- snp.excl$INFO  # copy to same col name as tm
-    snp.excl$Info <- snp.excl$INFO  # copy to same col name as tm
-    snp.excl$ref <- snp.excl$REF  # copy to same col name as tm
-  }
-  
+  # TODO: revisit and simplify now that both match
+  snp_excl$chr.pos <- paste0(snp_excl$CHROM, ":", snp_excl$POS)
+  snp_excl$FilterType <- snp_excl$INFO # copy to same col name as tm
+  snp_excl$Info <- snp_excl$INFO # copy to same col name as tm
+  snp_excl$ref <- snp_excl$REF # copy to same col name as tm
+
   # Load bim so can get actual varID names
   bim <- read.table(paste0(pre_qc_dir, "/pre_qc.bim"),
-                    stringsAsFactors = F)
+    stringsAsFactors = FALSE
+  )
   bim$chr.pos <- paste0(bim$V1, ":", bim$V4)
-  
+
   # Make sure presence/absence of chr matches bim file
   if (any(grepl("^chr", bim$V1))) {
-    snp.excl$chr.pos <- ifelse(
-      any(grepl("^chr", snp.excl$chr.pos)),
-      snp.excl$chr.pos,
-      paste0("chr", snp.excl$chr.pos))
+    snp_excl$chr.pos <- ifelse(
+      any(grepl("^chr", snp_excl$chr.pos)),
+      snp_excl$chr.pos,
+      paste0("chr", snp_excl$chr.pos)
+    )
   } else {
-    snp.excl$chr.pos <- gsub("^chr", "", snp.excl$chr.pos)
+    snp_excl$chr.pos <- gsub("^chr", "", snp_excl$chr.pos)
   }
-  
-  # Add varID names to snp.excl by chr:pos
-  merged <- merge(snp.excl, bim, by = c("chr.pos"))
-  
+
+  # Add varID names to snp_excl by chr:pos
+  merged <- merge(snp_excl, bim, by = c("chr.pos"))
+
   # Get strand flips & strand flip and allele switch
-  snp.frame.flip.as <- merged[grep("Strand flip", merged$FilterType),]
-  
-  # Remove strand flip & strand flip and allele switch from from snp.excl
-  merged.to.excl <- merged[!merged$FilterType %in% snp.frame.flip.as$FilterType,]
-  
+  snp_frame_flip_as <- merged[grep("Strand flip", merged$FilterType), ]
+
+  # Remove strand flip & strand flip and allele switch from from snp_excl
+  merged_to_excl <-
+    merged[!merged$FilterType %in% snp_frame_flip_as$FilterType, ]
+
   # Get allele switch only snps
-  snp.frame.as <- merged.to.excl[grep("Allele switch", merged.to.excl$FilterType),]
-  
+  snp_frame_as <-
+    merged_to_excl[grep("Allele switch", merged_to_excl$FilterType), ]
+
   ### Handle strand flips & strand flip and allele switches
   # For both strand flip & strand flip and allele switch, flip strand
   # Only do if strand flips were found, otherwise create empty list
-  if (nrow(snp.frame.flip.as) > 0) {
+  if (nrow(snp_frame_flip_as) > 0) {
     # Get varIDs from .bim by chr:pos for strand flip & strand flip and
     # allele switch
-    snps <- bim$V2[bim$chr.pos %in% snp.frame.flip.as$chr.pos]
-    
+    snps <- bim$V2[bim$chr.pos %in% snp_frame_flip_as$chr.pos]
+
     # Get ref alleles for strand flip and allele switch
-    snp.frame.flip.as.both <- snp.frame.flip.as[grep("Strand flip and Allele switch", snp.frame.flip.as$FilterType),]
-    snp.frame.flip.as.both$ref <- gsub("/[[:alpha:]]", "",
-                               gsub(".*:", "", snp.frame.flip.as.both$Info))
-    
+    snp_frame_flip_as_both <-
+      snp_frame_flip_as[
+        grep("Strand flip and Allele switch", snp_frame_flip_as$FilterType),
+      ]
+    snp_frame_flip_as_both$ref <- gsub(
+      "/[[:alpha:]]", "",
+      gsub(".*:", "", snp_frame_flip_as_both$Info)
+    )
+
     # Get varID name with reference a2 allele for PLINK --a2-allele
-    snp.frame.flip.as.both <- snp.frame.flip.as.both[,c("V2", "ref")]
-    
+    snp_frame_flip_as_both <- snp_frame_flip_as_both[, c("V2", "ref")]
   } else {
     snps <- character()
-    snp.frame.flip.as.both <- character()
+    snp_frame_flip_as_both <- character()
     print("No strand flip SNPs were found in the excluded SNPs.")
   }
-  
+
   # Write out SNPs for input into PLINK --flip, will flip both strand
   # flip & strand flip and allele switch
-  write.table(snps, paste0(post_qc_dir, "/tmp_flip.txt"), 
-              sep="\t", quote=F, row.names=F, col.names=F)
-  
+  write.table(snps, paste0(post_qc_dir, "/tmp_flip.txt"),
+    sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE
+  )
+
   # Write out SNPs for input into PLINK --a2-allele, will allele switch
   # strand flip and allele switch alleles after --flip
-  write.table(snp.frame.flip.as.both, paste0(post_qc_dir, "/tmp_a2-allele.txt"),
-              sep="\t", quote = F, row.names = F, col.names = F)
-  
+  write.table(
+    snp_frame_flip_as_both, paste0(post_qc_dir, "/tmp_a2-allele.txt"),
+    sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE
+  )
+
   ### Handle allele switches only
   # Get ref alleles for strand flip and allele switch
-  snp.frame.as$ref <- gsub("/[[:alpha:]]", "",
-                           gsub(".*:", "", snp.frame.as$Info))
-  
+  snp_frame_as$ref <- gsub(
+    "/[[:alpha:]]", "",
+    gsub(".*:", "", snp_frame_as$Info)
+  )
+
   # Get varID name with reference a2 allele for PLINK --a2-allele
-  snp.frame.as <- snp.frame.as[,c("V2", "ref")]
-  
+  snp_frame_as <- snp_frame_as[, c("V2", "ref")]
+
   # Write out SNPs for input into PLINK --a2-allele, will allele switch
   # alleles switches only
-  write.table(snp.frame.as, paste0(post_qc_dir, "/tmp_a2-allele_switch_only.txt"),
-              sep="\t", quote = F, row.names = F, col.names = F)
-  
+  write.table(
+    snp_frame_as, paste0(post_qc_dir, "/tmp_a2-allele_switch_only.txt"),
+    sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE
+  )
 }
 
 get_strand_flip_snp_names(args[1], args[2], args[3])
